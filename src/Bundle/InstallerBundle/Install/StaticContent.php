b0480a91fe7655b6c403a1eba97c7295ac35fad3
Marijn Otte <marijn@e-active.nl>
2020-07-07 10:09:55 +0200

    Further implement StaticContent

--- src/Bundle/InstallerBundle/Install/StaticContent.php
+++ src/Bundle/InstallerBundle/Install/StaticContent.php
@@ -2,42 +2,56 @@
 
 namespace Integrated\Bundle\InstallerBundle\Install;
 
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ORM\EntityManager;
 use Integrated\Bundle\ContentBundle\StaticContent\StaticContentRepository;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class StaticContent
 {
-    /**
-     * @var EntityManager
-     */
-    protected $entityManager;
-
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
-
     /**
      * @var StaticContentRepository
      */
     private $repository;
 
     /**
+     * @var DocumentManager
+     */
+    private $documentManager;
+
+    /**
      * Migrations constructor.
      *
-     * @param EntityManager           $entityManager
-     * @param ContainerInterface      $container
+     * @param DocumentManager         $documentManager
      * @param StaticContentRepository $repository
      */
-    public function __construct(EntityManager $entityManager, ContainerInterface $container, StaticContentRepository $repository)
+    public function __construct(DocumentManager $documentManager, StaticContentRepository $repository)
     {
-        $this->entityManager = $entityManager;
-        $this->container = $container;
+        $this->documentManager = $documentManager;
         $this->repository = $repository;
     }
 
     public function execute()
     {
+        $encoders = [new XmlEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        foreach ($this->repository->all() as $item) {
+            dump($item);
+            $document = $this->documentManager->getRepository($item['class'])->find($item['id']);
+            if ($document === null) {
+                dump($document['fields']);
+                $document = $serializer->deserialize($item['fields'], $item['class'], 'xml');
+
+                $this->documentManager->persist($document);
+            }
+
+            $this->documentManager->flush();
+        }
     }
 }
