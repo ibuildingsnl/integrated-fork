94abf35332cca4a6c5893eb625365f6912c96818
Marijn Otte <marijn@e-active.nl>
2020-07-14 10:44:34 +0200

    Improve serializer

--- src/Bundle/InstallerBundle/Install/StaticContent.php
+++ src/Bundle/InstallerBundle/Install/StaticContent.php
@@ -4,11 +4,15 @@ namespace Integrated\Bundle\InstallerBundle\Install;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ORM\EntityManager;
+use Integrated\Bundle\ContentBundle\Document\ContentType\ContentType;
+use Integrated\Bundle\ContentBundle\Document\Relation\Relation;
 use Integrated\Bundle\ContentBundle\StaticContent\StaticContentRepository;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class StaticContent
 {
@@ -23,32 +27,48 @@ class StaticContent
     private $documentManager;
 
     /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
      * Migrations constructor.
      *
      * @param DocumentManager         $documentManager
      * @param StaticContentRepository $repository
+     * @param SerializerInterface     $serializer
      */
-    public function __construct(DocumentManager $documentManager, StaticContentRepository $repository)
+    public function __construct(DocumentManager $documentManager, StaticContentRepository $repository, SerializerInterface $serializer)
     {
         $this->documentManager = $documentManager;
         $this->repository = $repository;
+        $this->serializer = $serializer;
     }
 
     public function execute()
     {
-        $encoders = [new XmlEncoder()];
-        $normalizers = [new ObjectNormalizer()];
+        //$encoders = [new XmlEncoder()];
+        //$normalizers = [new ObjectNormalizer()];
 
-        $serializer = new Serializer($normalizers, $encoders);
+        //$serializer = new Serializer($normalizers, $encoders);
+
+        //$rel = $this->documentManager->getRepository(Relation::class)->find('parent_company');
+        //dump($this->serializer->serialize($rel, 'xml'));
 
         foreach ($this->repository->all() as $item) {
-            dump($item);
             $document = $this->documentManager->getRepository($item['class'])->find($item['id']);
             if ($document === null) {
-                dump($document['fields']);
-                $document = $serializer->deserialize($item['fields'], $item['class'], 'xml');
+                $document = $this->serializer->deserialize($item['fields-xml'], $item['class'], 'xml');
+
+                if ($item['defaults-xml'] !== null) {
+                    $document = $this->serializer->deserialize($item['defaults-xml'], $item['class'], 'xml', [AbstractNormalizer::OBJECT_TO_POPULATE => $document]);
+                }
 
                 $this->documentManager->persist($document);
+            } else {
+                $this->serializer->deserialize($item['fields-xml'], $item['class'], 'xml', [
+                    AbstractNormalizer::OBJECT_TO_POPULATE => $document,
+                    ]);
             }
 
             $this->documentManager->flush();
---
+++ src/Bundle/InstallerBundle/Serializer/Normalizer/DocumentNormalizer.php
@@ -0,0 +1,138 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\InstallerBundle\Serializer\Normalizer;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Integrated\Bundle\ContentBundle\Document\ContentType\ContentType;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class DocumentNormalizer implements DenormalizerInterface
+{
+
+    /**
+     * @var DocumentManager
+     */
+    private $documentManager;
+
+    /**
+     * @var ObjectNormalizer
+     */
+    private $objectNormalizer;
+
+    const MAPPING = [
+        ContentType::class
+    ];
+
+    /**
+     * @param DocumentManager  $documentManager
+     * @param ObjectNormalizer $objectNormalizer
+     */
+    public function __construct(DocumentManager $documentManager)
+    {
+        $this->objectNormalizer = new ObjectNormalizer();
+        $this->documentManager = $documentManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        $conversion = [];
+        if ($this->supports($class)) {
+            $meta = $this->documentManager->getClassMetadata($class);
+
+            foreach ($data as $key => $value) {
+                if (!$meta->hasField($key)) {
+                    continue;
+                }
+
+                $fieldMapping = $meta->getFieldMapping($key);
+                if (!isset($fieldMapping['targetDocument'])) {
+                    continue;
+                }
+
+                if (!in_array($fieldMapping['targetDocument'], $this::MAPPING)) {
+                    continue;
+                }
+
+                $conversion[$key] = [
+                    'class' => $fieldMapping['targetDocument'],
+                    'ids' => is_array($data[$key]) ? $data[$key] : [$data[$key]],
+                ];
+
+                unset($data[$key]);
+            }
+
+        }
+
+        $document = $this->objectNormalizer->denormalize($data, $class, $format, $context);
+
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+
+        foreach ($conversion as $field => $data) {
+            $items = [];
+            dump($data);
+            foreach ($data['ids'] as $id) {
+                $items[] = $this->documentManager->getRepository($data['class'])->find($id);
+            }
+
+            $propertyAccessor->setValue($document, $field, $items);
+        }
+
+        return $document;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        if (!\is_array($data)) {
+            return false;
+        }
+
+        return $this->supports($type);
+    }
+
+    /**
+     * Check if the class is a mongodb document class registered by the
+     * registered document manager.
+     *
+     * @param $class
+     *
+     * @return bool
+     */
+    protected function supports($class)
+    {
+        $meta = $this->documentManager->getClassMetadata($class);
+
+        if ($meta->isMappedSuperclass || $meta->isEmbeddedDocument) {
+            return false;
+        }
+
+        $identifier = $meta->getIdentifierFieldNames();
+
+        if (empty($identifier)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function supportsdo($class)
+    {
+
+    }
+}
