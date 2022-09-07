082b211be57aabb3e0336715097dd5be0df222c2
Bas <bas@twindigital.nl>
2022-09-07 16:26:59 +0200

    Roundup of the day

--- src/Bundle/ContentBundle/Form/Type/ActionsType.php
+++ src/Bundle/ContentBundle/Form/Type/ActionsType.php
@@ -26,12 +26,12 @@ class ActionsType extends BaseActionsType
     {
         parent::__construct([
             'create' => ['type' => SubmitType::class, 'options' => ['label' => 'Create', 'button_class' => 'orange']],
-            'save' => ['type' => SubmitType::class, 'options' => ['label' => 'Save', 'button_class' => 'orange']],
+            'save' => ['type' => SubmitType::class, 'options' => ['label' => 'Update', 'button_class' => 'green']],
             'delete' => ['type' => SubmitType::class, 'options' => ['label' => 'Delete', 'button_class' => 'orange']],
             'back' => ['type' => SubmitType::class, 'options' => ['label' => 'Back', 'attr' => ['formnovalidate' => 'formnovalidate']]],
             'reload' => ['type' => SubmitType::class, 'options' => ['label' => 'Reload', 'button_class' => 'orange', 'attr' => ['formnovalidate' => 'formnovalidate']]],
             'reload_changed' => ['type' => SubmitType::class, 'options' => ['label' => 'Reload (keep changes)', 'attr' => ['formnovalidate' => 'formnovalidate']]],
-            'cancel' => ['type' => SubmitType::class, 'options' => ['label' => 'Cancel', 'button_class' => 'gray-thin', 'attr' => ['formnovalidate' => 'formnovalidate', 'data-dismiss' => 'modal']]],
+            'cancel' => ['type' => SubmitType::class, 'options' => ['label' => 'Back', 'button_class' => 'gray-thin', 'attr' => ['formnovalidate' => 'formnovalidate', 'data-dismiss' => 'modal']]],
         ]);
     }
 
