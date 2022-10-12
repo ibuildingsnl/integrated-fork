9b7914708fa30b2be6cb54b08216d28e1552fdc1
Bas <bas@twindigital.nl>
2022-10-12 17:25:59 +0200

    Tweak: Moved FontAwesome to webpack / import with yarn

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,9 @@ webpackConfig = Encore.setOutputPath(
     addEntry('app', [
         './src/Bundle/ContentBundle/Resources/assets/sass/main.scss',
         './src/Bundle/WorkflowBundle/Resources/assets/css/style.css',
-        './src/Bundle/ContentBundle/Resources/assets/js/main.js',
-    ]).
+        './node_modules/@fortawesome/fontawesome-pro/css/fontawesome.min.css',
+        './node_modules/@fortawesome/fontawesome-pro/css/regular.min.css',
+        './src/Bundle/ContentBundle/Resources/assets/js/main.js']).
     addEntry('edit', [
         './node_modules/jquery-datetimepicker/jquery.datetimepicker.css',
         './src/Bundle/ContentBundle/Resources/assets/js/edit.js',
@@ -19,34 +20,28 @@ webpackConfig = Encore.setOutputPath(
         './src/Bundle/FormTypeBundle/Resources/assets/css/tinymce.editor.css',
         './src/Bundle/FormTypeBundle/Resources/assets/js/editor.js',
         './node_modules/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css',
-        './node_modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js',
-    ]).
+        './node_modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js']).
     addEntry('iframe', [
-        './src/Bundle/BlockBundle/Resources/assets/css/iframe.css',
-    ]).
+        './src/Bundle/BlockBundle/Resources/assets/css/iframe.css']).
     addEntry('content_sortable', [
         './node_modules/components-jqueryui/jquery-ui.min.js',
-        './src/Bundle/FormTypeBundle/Resources/assets/js/content_sortable_collection.js',
-    ]).
+        './src/Bundle/FormTypeBundle/Resources/assets/js/content_sortable_collection.js']).
     addEntry('drag-drop', [
         './node_modules/jquery.filer/css/jquery.filer.css',
         './node_modules/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css',
         './node_modules/jquery.filer/js/jquery.filer.js',
         './src/Bundle/StorageBundle/Resources/assets/css/drag-drop.css',
-        './src/Bundle/StorageBundle/Resources/assets/js/drag-drop.js',
-    ]).
+        './src/Bundle/StorageBundle/Resources/assets/js/drag-drop.js']).
     addEntry('workflow', [
         './src/Bundle/WorkflowBundle/Resources/assets/js/updateTransitions.js',
         './src/Bundle/WorkflowBundle/Resources/assets/js/defaultSelection.js',
-        './src/Bundle/WorkflowBundle/Resources/assets/js/select2_init.js',
-    ]).
+        './src/Bundle/WorkflowBundle/Resources/assets/js/select2_init.js']).
     copyFiles({
-        from: './node_modules/tinymce/skins',
-        to: 'skins/[path][name].[ext]',
+        from: './node_modules/tinymce/skins', to: 'skins/[path][name].[ext]',
     }).
     copyFiles({
         from: './src/Bundle/ContentBundle/Resources/assets/images',
-        to: 'images/[path][name].[ext]'
+        to: 'images/[path][name].[ext]',
     }).
     cleanupOutputBeforeBuild().
     autoProvidejQuery().
@@ -56,7 +51,6 @@ webpackConfig = Encore.setOutputPath(
     disableSingleRuntimeChunk().
     enablePostCssLoader((options) => {
         options.postcssOptions = {
-            // directory where the postcss.config.js file is stored
             path: './postcss.config.js',
         };
     }).
@@ -66,7 +60,7 @@ webpackConfig.resolve.alias = {
     typeahead: PathResolver.resolve(__dirname,
         'node_modules/typeahead.js/dist/typeahead.bundle.js'),
     jquery: PathResolver.resolve(__dirname,
-        'node_modules/jquery/dist/jquery.js')
+        'node_modules/jquery/dist/jquery.js'),
 };
 
 webpackConfig.resolve.fallback = {'fs': false};
