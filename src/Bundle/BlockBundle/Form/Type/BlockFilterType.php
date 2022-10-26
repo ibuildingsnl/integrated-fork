9c2c502ab561e7807b1c7f245ab2b13e884e5270
Bas <bas@twindigital.nl>
2022-10-26 16:39:41 +0200

    php-cs-fixer

--- src/Bundle/BlockBundle/Form/Type/BlockFilterType.php
+++ src/Bundle/BlockBundle/Form/Type/BlockFilterType.php
@@ -68,7 +68,7 @@ class BlockFilterType extends AbstractType
             TextType::class,
             [
                 'required' => false,
-                'label' => 'Filter by block name'
+                'label' => 'Filter by block name',
             ]
         );
 
