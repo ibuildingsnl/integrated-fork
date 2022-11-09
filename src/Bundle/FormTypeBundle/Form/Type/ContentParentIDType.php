9088225549edf5e389927ea9381a49dc88803e5c
Wouter Koppers <wouter@twindigital.nl>
2022-11-09 12:45:13 +0100

    Processed PR comments, fixed dropdown not working of Media_Taxonomy. Added a js check if object exists in edit.html.twig

--- src/Bundle/FormTypeBundle/Form/Type/ContentParentIDType.php
+++ src/Bundle/FormTypeBundle/Form/Type/ContentParentIDType.php
@@ -87,6 +87,6 @@ class ContentParentIDType extends AbstractType
      */
     public function getBlockPrefix()
     {
-        return 'integrated_content_parent_id';
+        return 'integrated_content_rank';
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,6 @@ webpackConfig = Encore.setOutputPath(
         './src/Bundle/ContentBundle/Resources/assets/js/collection.js',
     ])
     .addEntry('mediagallery', [
-        './src/Bundle/ContentBundle/Resources/assets/sass/main.scss',
-        './src/Bundle/WorkflowBundle/Resources/assets/css/style.css',
-        './src/Bundle/ContentBundle/Resources/assets/js/main.js',
         './src/Bundle/ContentBundle/Resources/assets/js/jqueryui.js',
         './src/Bundle/ContentBundle/Resources/assets/css/mediaGallery.css',
         './src/Bundle/ContentBundle/Resources/assets/js/mediaGallery.js',
