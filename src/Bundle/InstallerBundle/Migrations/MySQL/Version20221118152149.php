c06f8d7735b1666a650a0d320c54325f522f1b0e
Bas <bas@twindigital.nl>
2022-11-18 16:29:38 +0100

    MySQL Migration - Color / Icon for Workflow state

---
+++ src/Bundle/InstallerBundle/Migrations/MySQL/Version20221118152149.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations\Doctrine;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221118152149 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE workflow_definition_states CHANGE color color VARCHAR(255) NOT NULL, CHANGE icon icon VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE workflow_definition_states CHANGE color color VARCHAR(120) CHARACTER SET utf8mb3 DEFAULT NULL COLLATE `utf8mb3_unicode_ci`, CHANGE icon icon VARCHAR(120) CHARACTER SET utf8mb3 DEFAULT NULL COLLATE `utf8mb3_unicode_ci`');
+    }
+}
