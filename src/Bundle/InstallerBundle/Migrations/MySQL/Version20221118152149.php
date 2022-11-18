1dc3ed3b574414f0ec4c38ae6f48f4f7062f7b01
Bas <bas@twindigital.nl>
2022-11-18 16:49:12 +0100

    php-cs-fixer

--- src/Bundle/InstallerBundle/Migrations/MySQL/Version20221118152149.php
+++ src/Bundle/InstallerBundle/Migrations/MySQL/Version20221118152149.php
@@ -9,19 +9,19 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20221118152149 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE workflow_definition_states CHANGE color color VARCHAR(255) NOT NULL, CHANGE icon icon VARCHAR(255) NOT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
