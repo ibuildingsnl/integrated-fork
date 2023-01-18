d60d93f6b6cb6ffa6cff32cf8b2b439d1b4f3f3a
Bas <bas@twindigital.nl>
2023-01-18 15:44:11 +0100

    Tweak: Added TinyMCE & Select2 init to append function

--- src/Bundle/ContentBundle/Resources/assets/js/collection.js
+++ src/Bundle/ContentBundle/Resources/assets/js/collection.js
@@ -30,6 +30,47 @@ $('[data-prototype]').each(function(index, elm) {
             $collection.data('index', index + 1);
             $collection.find('> ul').append(item);
 
+            initContentChoice();
+
+            let editor = $collection.find('ul li:last-child').find('textarea.integrated_tinymce');
+
+            if (editor) {
+                let style_formats = [
+                    {title: 'Paragraph', format: 'p'},
+                    {title: 'Heading 2', block: 'h2' },
+                    {title: 'Heading 3', block: 'h3' },
+                    {title: 'Heading 4', block: 'h4' },
+                    {title: 'Heading 5', block: 'h5' },
+                    {title: 'Preformatted (fixed font)', block: 'pre' },
+                    {title: 'Superscript', icon: "superscript", inline: 'sup'},
+                    {title: 'Subscript', icon: "subscript", inline: 'sub'}
+                ];
+
+                style_formats = style_formats.concat(editor.data('format_styles'));
+                tinymce.init({
+                    selector: '#' + editor.attr('id'),
+                    plugins:
+                        "advlist autolink link lists charmap anchor pagebreak " +
+                        "searchreplace wordcount visualchars fullscreen nonbreaking " +
+                        "table directionality template wordcount autoresize code",
+                    schema: "html5",
+                    menubar: true,
+                    branding: false,
+                    toolbar:
+                        "styles | bold italic underline | bullist numlist | " +
+                        "link anchor integratedImage integratedVideo integratedColumn image media print preview fullpage table | " +
+                        "charmap pagebreak | pastetext searchreplace | code fullscreen",
+                    browser_spellcheck : true,
+                    convert_urls: false,
+                    content_css: editor.data('content_css'),
+                    integrated_browser_media_types_url: editor.data('integrated_browser_media_types_url'),
+                    integrated_browser_search_url: editor.data('integrated_browser_search_url'),
+                    integrated_browser_file_url: editor.data('integrated_browser_file_url'),
+                    integrated_browser_file_resize_url: editor.data('integrated_browser_file_resize_url'),
+                    document_base_url : editor.data('document_base_url'),
+                    style_formats: style_formats
+                });
+            }
             register($collection.find('ul li:last-child'));
         }
 
@@ -39,6 +80,8 @@ $('[data-prototype]').each(function(index, elm) {
 
                 elm.remove();
             })
+
+
             elm.find('div.panel-heading').click( function() {
                 if ($(this).parent().find('.panel-collapse').hasClass('in')) {
                     $(this).parent().find('.panel-collapse').removeClass('in');
