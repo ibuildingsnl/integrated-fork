ec8ab40d9d20eb64212596f989d5266477c6b4c8
jesse <jesse@twindigital.nl>
2023-01-31 13:51:31 +0100

    Trying to get phpdoc_separation to be quiet

--- src/Bundle/ContentBundle/Document/Block/FormBlock.php
+++ src/Bundle/ContentBundle/Document/Block/FormBlock.php
@@ -44,11 +44,6 @@ class FormBlock extends Block
     #[Type\Field(type: 'Symfony\Component\Form\Extension\Core\Type\TextareaType', options: ['required' => false])]
     protected string $textAfterSubmit;
 
-    /**
-     * @Assert\All({
-     *     @Assert\Email
-     * })
-     */
     #[Type\Field(type: 'Integrated\Bundle\FormTypeBundle\Form\Type\TailwindCollectionType', options: [
         'label' => 'Sent form to e-mail address(es)',
         'entry_type' => 'Symfony\Component\Form\Extension\Core\Type\EmailType',
@@ -56,6 +51,11 @@ class FormBlock extends Block
         'allow_delete' => true,
         'required' => false,
     ])]
+    /**
+     * @Assert\All({
+     *     @Assert\Email
+     * })
+     */
     protected array $emailAddresses = [];
 
     #[Type\Field(type: 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', options: [
