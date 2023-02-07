9c644c838a1ce1329ed1eedee301a643c60bc720
jesse <jesse@twindigital.nl>
2023-02-07 11:58:07 +0100

    Cs fixer

--- src/Common/Channel/Connector/ExporterInterface.php
+++ src/Common/Channel/Connector/ExporterInterface.php
@@ -21,8 +21,8 @@ interface ExporterInterface
     public const STATE_DELETE = 'delete';
 
     /**
-     * @param object           $content
-     * @param string           $state
+     * @param object $content
+     * @param string $state
      *
      * @return ExporterResponse|null
      */
