f9ff314764db358d47fcdecc3644c64690ead3a4
Jan Sanne <jansanne@e-active.nl>
2023-02-14 14:51:28 +0100

    INTEGRATED-1779 refactored the tinymce integrated browser to support version 6 of tinymce

---
+++ src/Bundle/FormTypeBundle/Resources/assets/js/tinymce-integrated-browser/plugin.js
@@ -0,0 +1,9 @@
+import tinymce from 'tinymce';
+
+import * as Options from './api/options';
+import * as Buttons from './ui/buttons';
+
+tinymce.PluginManager.add('integratedbrowser', (editor) =>  {
+    Options.register(editor);
+    Buttons.register(editor);
+})
