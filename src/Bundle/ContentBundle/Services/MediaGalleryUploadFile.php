0deae53ecab986d4d446cb81b2cf4e823eaa8473
Bas <bas@twindigital.nl>
2023-02-17 12:50:36 +0100

    How could we forget?

--- src/Bundle/ContentBundle/Services/MediaGalleryUploadFile.php
+++ src/Bundle/ContentBundle/Services/MediaGalleryUploadFile.php
@@ -46,7 +46,7 @@ class MediaGalleryUploadFile
         // https://gist.github.com/tylerlee/53609bff1346cebf8f0a85b6be29a88e
         $uploadedFileMimetype = $request->files->get('file')->getMimeType();
         // TODO: perfect these filetypes, maybe put these in a config file?:
-        $image_filetypes = ['jpg', 'jpeg', 'png', 'tif', 'webp'];
+        $image_filetypes = ['jpg', 'jpeg', 'png', 'tif', 'webp', 'svg', 'gif'];
         $video_filetypes = ['mp4', 'mov', 'avi', 'flv', 'mkv', 'wmv'];
         $file_filetypes = ['doc', 'docx', 'pdf', 'xls', 'xlsx'];
 
