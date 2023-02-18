adbb91a5b8100cf11683693d4c6874588f48893f
Bas <bas@twindigital.nl>
2023-02-18 11:53:56 +0100

    Tweak: Added styling and used Media Library for logo

--- src/Bundle/NewsletterBundle/Document/Newsletter.php
+++ src/Bundle/NewsletterBundle/Document/Newsletter.php
@@ -3,6 +3,8 @@
 namespace Integrated\Bundle\NewsletterBundle\Document;
 
 use Integrated\Bundle\ContentBundle\Document\Content\Content;
+use Integrated\Bundle\ContentBundle\Document\Content\Image;
+use Integrated\Bundle\ContentBundle\Form\Type\MediaGalleryImageType;
 use Integrated\Bundle\ContentBundle\Form\Type\SocialsType;
 use Integrated\Bundle\NewsletterBundle\Document\Schedule\RecurringScheduleEntry;
 use Integrated\Bundle\NewsletterBundle\Form\ContentSelectionsType;
@@ -16,28 +18,76 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 #[Type\Document('Newsletter')]
 class Newsletter extends Content
 {
-    #[Type\Field]
+    /**
+     * @var string
+     */
+    #[Type\Field(options: ['priority' => 990, 'attr' => ['style' => 'editor', 'state' => 'show']], location: 'editor')]
     public string $title = 'untitled';
 
     /** @var string[] */
-    #[Type\Field(type: TestEmailAddressesType::class)]
+    #[Type\Field(type: TestEmailAddressesType::class, options: [
+        'priority' => 500,
+        'attr' => ['style' => 'sidebar', 'icon' => 'link', 'show_headings' => 'false']
+    ], location: 'sidebar')]
     public array $testAddresses = [];
 
-    #[Type\Field(type: IntegerType::class)]
+    #[Type\Field(type: IntegerType::class, options: [
+        'label' => 'Generation time',
+        'priority' => 490,
+        'attr' => [
+            'style' => 'sidebar',
+            'icon' => 'link',
+            'show_headings' => 'false',
+            'help_text' => 'Email is generated the amount of time given before sending.',
+        ]
+    ], location: 'sidebar')]
     public int $hoursBefore;
 
-    #[Type\Field(type: RecurringScheduleEntryType::class)]
+    #[Type\Field(type: RecurringScheduleEntryType::class, options: [
+        'label' => 'Sending Schedule',
+        'priority' => 490,
+        'attr' => [
+            'style' => 'editor',
+            'show_headings' => 'false',
+            'state' => 'show',
+        ]
+    ], location: 'editor')]
     public RecurringScheduleEntry $schedule;
 
-    #[Type\Field(type: ImageDropzoneType::class)]
-    public ?StorageInterface $logo = null;
+    #[Type\Field(type: MediaGalleryImageType::class, options: [
+        'label' => 'Logo',
+        'attr' => [
+            'style' => 'sidebar',
+            'icon' => 'media-image',
+            'data-types' => '[{"type":"image","name":"Image"}]',
+            'data-emptytext' => 'Select logo',
+            'data-multiple' => false,
+        ],
+    ], location: 'sidebar')]
+    public ?Image $logo = null;
 
     /** @var string[] */
-    #[Type\Field(type: ContentSelectionsType::class)]
+    #[Type\Field(type: ContentSelectionsType::class, options: [
+        'label' => 'Content selection',
+        'priority' => 480,
+        'attr' => [
+            'style' => 'editor',
+            'show_headings' => 'false',
+            'state' => 'show',
+        ]
+    ], location: 'editor')]
     public array $contentSelection = [];
 
     /** @var string[] */
-    #[Type\Field(type: SocialsType::class)]
+    #[Type\Field(type: SocialsType::class, options: [
+        'label' => 'Socials',
+        'priority' => 470,
+        'attr' => [
+            'style' => 'editor',
+            'show_headings' => 'false',
+            'state' => 'show',
+        ]
+    ], location: 'editor')]
     public array $socials;
 
     public function __toString()
