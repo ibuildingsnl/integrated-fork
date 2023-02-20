16ae60fe8203adad3eef7a0cd4842a67376dfbb6
Jeroen van Leeuwen <jeroen@e-active.nl>
2023-02-20 08:25:11 +0100

    INTEGRATED-1830 Added security functions for "website"

---
+++ src/Bundle/UserBundle/Controller/Website/SecurityController.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\UserBundle\Controller\Website;
+
+use Integrated\Bundle\UserBundle\Doctrine\UserManager;
+use Integrated\Bundle\UserBundle\Form\Type\LoginFormType;
+use Integrated\Bundle\UserBundle\Form\Type\PasswordChangeType;
+use Integrated\Bundle\UserBundle\Form\Type\PasswordResetType;
+use Integrated\Bundle\UserBundle\Service\KeyGenerator;
+use Integrated\Bundle\UserBundle\Service\Mailer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Error\Error;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @var UserManager
+     */
+    private $userManager;
+
+    /**
+     * @var Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var KeyGenerator
+     */
+    private $keyGenerator;
+
+    public function __construct(UserManager $userManager, Mailer $mailer, KeyGenerator $keyGenerator)
+    {
+        $this->userManager = $userManager;
+        $this->mailer = $mailer;
+        $this->keyGenerator = $keyGenerator;
+    }
+
+    /**
+     * @return Response
+     */
+    public function login()
+    {
+        $form = $this->createForm(
+            LoginFormType::class,
+            null,
+            ['action' => $this->generateUrl('integrated_user_website_security_check')]
+        );
+
+        return $this->render('@IntegratedUser/website/security/login.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @return RedirectResponse|Response
+     */
+    public function passwordReset(Request $request)
+    {
+        $form = $this->createForm(
+            PasswordResetType::class,
+            null,
+            ['action' => $this->generateUrl('integrated_user_website_security_password_reset')]
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($user = $this->userManager->findEnabledByUsernameAndScope($form->get('email')->getData(), $request->attributes->get('scope'))) {
+                if ($user->isEnabled()) {
+                    $this->mailer->sendPasswordResetMail($user, true);
+                }
+            }
+
+            $this->addFlash('success', 'If your e-mail address has an account, a password reset link has been sent');
+
+            return $this->redirectToRoute('integrated_user_website_security_login');
+        }
+
+        return $this->render('@IntegratedUser/website/security/password_reset.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @return RedirectResponse|Response
+     *
+     * @throws Error
+     */
+    public function passwordChange(Request $request, int $id, int $timestamp, string $key)
+    {
+        if (!$this->keyGenerator->isValidKey($id, $timestamp, $key)) {
+            $this->addFlash('danger', 'Password reset link is invalid or expired');
+
+            return $this->redirectToRoute('integrated_user_website_security_password_reset');
+        }
+
+        $form = $this->createForm(
+            PasswordChangeType::class,
+            null,
+            ['action' => $this->generateUrl('integrated_user_website_security_password_change', ['id' => $id, 'timestamp' => $timestamp, 'key' => $key])]
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($this->userManager->changePassword($id, $form->get('password')->getData())) {
+                $this->addFlash('success', 'Your password has been changed');
+            } else {
+                $this->addFlash('danger', 'An error occurred while changing the password');
+            }
+
+            return $this->redirectToRoute('integrated_user_website_security_login');
+        }
+
+        return $this->render('@IntegratedUser/website/security/password_reset.html.twig', ['form' => $form->createView()]);
+    }
+}
---
+++ src/Bundle/UserBundle/Handler/Exception/UniqueUserException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Integrated\Bundle\UserBundle\Handler\Exception;
+
+class UniqueUserException extends \Exception
+{
+}
