68b99fbd26a47825a3db00c36cd9148894ff93c9
Wouter Koppers <wouter@twindigital.nl>
2023-02-23 16:43:33 +0100

    Git is working again, ran all fixers

--- src/Bundle/ContentBundle/Controller/ContentController.php
+++ src/Bundle/ContentBundle/Controller/ContentController.php
@@ -680,12 +680,6 @@ class ContentController extends AbstractController
             // not lost and there is a new change to get a lock on the content.
         }
 
-        if ($request->get('_route') == 'integrated_content_content_edit_iframe') {
-            $renderTo = '@IntegratedContent/content/edit.iframe.html.twig';
-        } else {
-            $renderTo = '@IntegratedContent/content/edit.html.twig';
-        }
-
         if ($locking['locked']) {
             // the document is locked so display display a error message explaining that
             // the user can not edit this page will the lock is there.
@@ -711,6 +705,12 @@ class ContentController extends AbstractController
             }
 
             $this->addFlash('danger', $text);
+        }
+
+        if ($request->get('_route') == 'integrated_content_content_edit_iframe') {
+            $renderTo = '@IntegratedContent/content/edit.iframe.html.twig';
+        } else {
+            $renderTo = '@IntegratedContent/content/edit.html.twig';
         }
 
         return $this->render($renderTo, [
