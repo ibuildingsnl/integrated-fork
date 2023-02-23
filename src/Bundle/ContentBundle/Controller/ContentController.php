fd115075a067f9dd21b01fbaaf16d20002a50375
Wouter Koppers <wouter@twindigital.nl>
2023-02-23 12:22:27 +0100

    Redirecting to its own route

--- src/Bundle/ContentBundle/Controller/ContentController.php
+++ src/Bundle/ContentBundle/Controller/ContentController.php
@@ -583,10 +583,8 @@ class ContentController extends AbstractController
     {
         if ($request->getRequestFormat() == 'iframe.html') {
             $renderTo = '@IntegratedContent/content/edit.iframe.html.twig';
-            $route = 'integrated_content_content_edit_iframe';
         } else {
             $renderTo = '@IntegratedContent/content/edit.html.twig';
-            $route = 'integrated_content_content_edit';
         }
 
         /** @var ContentTypeInterface $contentType */
@@ -614,7 +612,7 @@ class ContentController extends AbstractController
                             'lock' => $locking['lock']->getId(),
                         ]);
 
-                        return $this->redirectToRoute($route, $parameters);
+                        return $this->redirectToRoute($request->get('_route'), $parameters);
                     }
 
                     $locking['locked'] = false;
@@ -644,7 +642,7 @@ class ContentController extends AbstractController
             }
 
             if ($form->get('actions')->getData() == 'reload') {
-                return $this->redirectToRoute($route, ['id' => $content->getId()]);
+                return $this->redirectToRoute($request->get('_route'), ['id' => $content->getId()]);
             }
 
             // this is not rest compatible since a button click is required to save
@@ -676,7 +674,7 @@ class ContentController extends AbstractController
                     }
                 }
 
-                return $this->redirectToRoute($route, ['id' => $content->getId()]);
+                return $this->redirectToRoute($request->get('_route'), ['id' => $content->getId()]);
             }
             // reload_changed is just submitting without saving so the changes made are
             // not lost and there is a new change to get a lock on the content.
@@ -1145,15 +1143,9 @@ class ContentController extends AbstractController
             $parameters = array_merge($request->query->all(), $parameters);
         }
 
-        if ($request->getRequestFormat() == 'iframe.html') {
-            $route = 'integrated_content_content_edit_iframe';
-        } else {
-            $route = 'integrated_content_content_edit';
-        }
-
         $options = [
             'action' => $this->generateUrl(
-                $route,
+                $request->get('_route'),
                 $parameters
             ),
             'method' => 'PUT',
