ca52a564feac7b2aae9d9151e03d6e5c5cb60491
Bas <bas@twindigital.nl>
2023-03-08 21:21:05 +0100

    TODO

--- src/Bundle/ContentBundle/Controller/ContentController.php
+++ src/Bundle/ContentBundle/Controller/ContentController.php
@@ -522,7 +522,7 @@ class ContentController extends AbstractController
                 $this->addFlash('success', $this->getTranslator()->trans('The document %name% has been created', ['%name%' => $contentType->getName()]));
 
                 return $this->redirectToRoute('integrated_content_content_edit', ['id' => $content->getId(), 'remember' => 1]);
-                // TODO: Test if remember works. Expected result; when creating a new article, and move back to content navigator (with the back button) your search should still be intact
+                // TODO: Remember is broken, needs fixin.
             }
         }
 
