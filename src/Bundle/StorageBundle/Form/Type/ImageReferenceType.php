d7394cf908093803b463ec0a525af28a9533a4bd
koen <koen@e-active.nl>
2023-03-28 15:04:19 +0200

    INTEGRATED-1852 added new image type that adds content images

---
+++ src/Bundle/StorageBundle/Form/Type/ImageReferenceType.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\StorageBundle\Form\Type;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Integrated\Bundle\ContentBundle\Document\Channel\Channel;
+use Integrated\Bundle\StorageBundle\Form\Mapper\ImageReferenceMapper;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageReferenceType extends AbstractType
+{
+    public function __construct(
+        private readonly DocumentManager $manager
+    ) {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('image', ImageType::class);
+
+        $builder->setDataMapper(new ImageReferenceMapper($this->manager, $options['channels']));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('channels', []);
+
+        $resolver->addAllowedTypes('channels', ['array']);
+        $resolver->setAllowedValues('channels', function ($channels) {
+            foreach ($channels as $channel) {
+                if (!$channel instanceof Channel) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+        parent::configureOptions($resolver);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'integrated_image_reference';
+    }
+}
