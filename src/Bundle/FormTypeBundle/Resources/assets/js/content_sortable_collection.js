ac5aa73c20120050266e6f857ad6296aec7cf8c2
Bas <bas@twindigital.nl>
2023-04-04 22:27:48 +0200

    Tweak: no-panel mode for Sortable Collection

--- src/Bundle/FormTypeBundle/Resources/assets/js/content_sortable_collection.js
+++ src/Bundle/FormTypeBundle/Resources/assets/js/content_sortable_collection.js
@@ -8,4 +8,12 @@ $(function() {
             $(element).find('input[data-itemorder="collection"]').val(i);
         });
     });
+
+    $('.sortable-collection').on('sortupdate', function() {
+        // Loop through each <select> element and update its "name" attribute
+        $('ul.ui-sortable li select').each(function(index) {
+            var newName = $(this).attr('name').replace(/\[\d+\]/, '[' + index + ']');
+            $(this).attr('name', newName);
+        });
+    });
 });
