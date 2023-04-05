515a1238af3320206eefb7e88d5fb6f2e2fca460
Bas <bas@twindigital.nl>
2023-04-05 21:46:56 +0200

    Jenkins

--- src/Bundle/ContentBundle/Form/Type/SocialType.php
+++ src/Bundle/ContentBundle/Form/Type/SocialType.php
@@ -12,7 +12,6 @@
 namespace Integrated\Bundle\ContentBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
