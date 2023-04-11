ca4e315adeb3495722cde18246d5a7b966d34c84
Bas <bas@twindigital.nl>
2023-04-11 10:39:41 +0200

    Fix: wrong selector after inserting Gallery

--- src/Bundle/FormTypeBundle/Resources/assets/js/tinymce-integrated-browser/ui/dialog.js
+++ src/Bundle/FormTypeBundle/Resources/assets/js/tinymce-integrated-browser/ui/dialog.js
@@ -41,9 +41,9 @@ const galleryHandler = (editor, dialog, data) => {
 
     editor.insertContent(Templates.gallery(data.images));
 
-    function addRemoveButton(element) {
+    function addRemoveButton(element, className) {
         const removeButton = editor.contentDocument.createElement('span');
-        removeButton.classList.add('swiper-append', 'remove');
+        removeButton.classList.add(className, 'remove');
         removeButton.innerHTML = '<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>';
 
         removeButton.addEventListener('click', function() {
@@ -53,16 +53,20 @@ const galleryHandler = (editor, dialog, data) => {
         element.appendChild(removeButton);
     }
 
-    const swiperSlides = editor.contentDocument.querySelectorAll('.swiper-slide');
-    const articleSwiper = editor.contentDocument.querySelectorAll('.article-swiper');
+    function addRemoveButtons() {
+        const swiperSlides = editor.contentDocument.querySelectorAll('.swiper-slide');
+        const articleSwiper = editor.contentDocument.querySelectorAll('.article-swiper');
 
-    articleSwiper.forEach(function(swiper) {
-        addRemoveButton(swiper);
-    });
+        articleSwiper.forEach(function(swiper) {
+            addRemoveButton(swiper, 'swiper-append');
+        });
 
-    swiperSlides.forEach(function(slide) {
-        addRemoveButton(slide);
-    });
+        swiperSlides.forEach(function(slide) {
+            addRemoveButton(slide, 'slider-append');
+        });
+    }
+
+    addRemoveButtons();
 
     dialog.close();
 };
