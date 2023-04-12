09ef2bfc86e302d87e39a21bc1cfae2d3fa6a532
Bas <bas@twindigital.nl>
2023-04-12 13:08:45 +0200

    Tweak: simplified handlebars and checked for existing remove buttons

--- src/Bundle/FormTypeBundle/Resources/assets/js/tinymce-integrated-browser/ui/dialog.js
+++ src/Bundle/FormTypeBundle/Resources/assets/js/tinymce-integrated-browser/ui/dialog.js
@@ -42,15 +42,21 @@ const galleryHandler = (editor, dialog, data) => {
     editor.insertContent(Templates.gallery(data.images));
 
     function addRemoveButton(element, className) {
-        const removeButton = editor.contentDocument.createElement('span');
-        removeButton.classList.add(className, 'remove');
-        removeButton.innerHTML = '<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+        const removeButton = element.querySelector(`:scope > .remove`);
+        if (removeButton) {
+            // If a "remove" button already exists, do nothing
+            return;
+        }
 
-        removeButton.addEventListener('click', function() {
+        const newRemoveButton = editor.contentDocument.createElement('span');
+        newRemoveButton.classList.add(className, 'remove');
+        newRemoveButton.innerHTML = '<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+
+        newRemoveButton.addEventListener('click', function() {
             element.parentNode.removeChild(element);
         });
 
-        element.appendChild(removeButton);
+        element.appendChild(newRemoveButton);
     }
 
     function addRemoveButtons() {
