fc1fc4471eec59a7a44966cbcd44da5679facf16
Bas <bas@twindigital.nl>
2023-04-21 13:11:12 +0200

    Jenkins

--- src/Bundle/ContentBundle/Document/Content/Article.php
+++ src/Bundle/ContentBundle/Document/Content/Article.php
@@ -100,7 +100,7 @@ class Article extends Content implements RankableInterface
      */
     #[Type\Field(options: [
         'priority' => 450,
-        'attr' => ['style' => 'sidebar', 'icon' => 'megaphone']
+        'attr' => ['style' => 'sidebar', 'icon' => 'megaphone'],
     ], location: 'sidebar')]
     protected $source;
 
@@ -488,6 +488,6 @@ class Article extends Content implements RankableInterface
      */
     public function __toString()
     {
-        return (string)$this->title;
+        return (string) $this->title;
     }
 }
