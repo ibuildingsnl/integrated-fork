ed8ff648182d2d9c6ac658915eb4dbe9873da05b
jesse <jesse@twindigital.nl>
2023-04-26 19:26:45 +0200

    Fix pagination limit type

--- src/Bundle/ContentBundle/Controller/ContentController.php
+++ src/Bundle/ContentBundle/Controller/ContentController.php
@@ -165,7 +165,7 @@ class ContentController extends AbstractController
                     $options['end'] = $options['start']->add(\DateInterval::createFromDateString('1 week'));
                     unset($options['week']);
                     $request->query->set('page', 1);
-                    $request->query->set('limit', INF);
+                    $request->query->set('limit', PHP_INT_MAX);
                     break;
                 case 'month':
                     $view = '_month';
@@ -173,7 +173,7 @@ class ContentController extends AbstractController
                     $options['end'] = $options['start']->add(\DateInterval::createFromDateString('1 month'));
                     unset($options['month']);
                     $request->query->set('page', 1);
-                    $request->query->set('limit', INF);
+                    $request->query->set('limit', PHP_INT_MAX);
                     break;
             }
         }
