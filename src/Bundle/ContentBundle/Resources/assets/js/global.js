83348cba4edcd8c95a8991f87fb149949164ceba
Bas <bas@twindigital.nl>
2023-04-28 15:04:26 +0200

    Also clear dropdown_overlay contents with esc

--- src/Bundle/ContentBundle/Resources/assets/js/global.js
+++ src/Bundle/ContentBundle/Resources/assets/js/global.js
@@ -271,6 +271,7 @@ function openSelectedOptions(elem) {
 function toggleDropDownBackGround(hidden) {
     let menuItemDropDownUnderlay =  document.querySelector('#dropdown_overlay');
     menuItemDropDownUnderlay.classList.toggle('hide', hidden);
+    menuItemDropDownUnderlay.innerHTML = '';
 }
 
 function isElement(o) {
