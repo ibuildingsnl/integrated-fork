7db3af4600a83eebc6bc92f7176f07d467277e18
Bas <bas@twindigital.nl>
2023-07-08 11:03:00 +0200

    Fix for fatal error on Copy Pages

--- src/Bundle/PageBundle/Services/PageCopyService.php
+++ src/Bundle/PageBundle/Services/PageCopyService.php
@@ -102,6 +102,7 @@ class PageCopyService
             $block = $item->getBlock();
 
             if ($block instanceof Block) {
+
                 // copy block
                 if (isset($data['block_'.$block->getId()]['operation']) && $data['block_'.$block->getId()]['operation'] == 'clone') {
                     $copiedBlock = clone $block;
