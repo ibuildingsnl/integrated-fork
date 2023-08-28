89e8b53824524402ec7f88fec35e198fe9bb4486
Bas <bas@twindigital.nl>
2023-08-28 14:10:00 +0200

    Test fix

--- src/Bundle/ContentBundle/Tests/Document/Content/ArticleTest.php
+++ src/Bundle/ContentBundle/Tests/Document/Content/ArticleTest.php
@@ -170,7 +170,8 @@ class ArticleTest extends ContentTest
         $this->article->setAddress(new Address());
 
         $location = new Location();
-        $this->assertSame($location, $this->article->getAddress()->setLocation($location)->getLocation());
+        $this->article->getAddress()->setLocation($location);
+        $this->assertSame($location, $this->article->getAddress()->getLocation());
     }
 
     /**
