6d468afc80c8e07640c02683aa6d64b5a7b36fa2
Bas <bas@twindigital.nl>
2023-09-11 11:49:46 +0200

    Remove comment

--- src/Bundle/SitemapBundle/Controller/NewsController.php
+++ src/Bundle/SitemapBundle/Controller/NewsController.php
@@ -74,8 +74,6 @@ class NewsController extends AbstractController
             ->getQuery()
             ->getIterator();
 
-        // TODO: $this->getParameter('locale') is broken
-
         return [
             'channel' => $channel,
             'locale' => $this->getParameter('kernel.default_locale'),
