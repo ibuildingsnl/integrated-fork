ac6b994b88e71054c1104728f1d71623fbe20311
Bas <bas@twindigital.nl>
2023-09-15 21:26:59 +0200

    Used default classes to avoid overlap

--- src/Bundle/BrandBundle/Form/Type/TwitterExampleType.php
+++ src/Bundle/BrandBundle/Form/Type/TwitterExampleType.php
@@ -13,7 +13,7 @@ class TwitterExampleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('time', PublishTimeType::class);
+        $builder->add('time', PublishTimeType::class, ['label' => false]);
         $builder->add('text', TextareaType::class);
     }
 }
