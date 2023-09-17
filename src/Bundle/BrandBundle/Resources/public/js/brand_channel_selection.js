50e7689ac6ac13ef603ec34c4ee7670fbb439b03
Bas <bas@twindigital.nl>
2023-09-17 12:53:40 +0200

    Added search functionality

--- src/Bundle/BrandBundle/Resources/public/js/brand_channel_selection.js
+++ src/Bundle/BrandBundle/Resources/public/js/brand_channel_selection.js
@@ -6,6 +6,45 @@ document.querySelectorAll('.brands input.brand-choice').forEach(
     ) || brandCheckbox.dispatchEvent(new Event('change'))
 );
 
+document.addEventListener("DOMContentLoaded", function() {
+    const brandsDiv = document.querySelector('.brands .aside-item-list');
+    const htmlContent = `
+        <div class="aside-item-search">
+            <i class="iconoir-search"></i>
+            <input type="text" class="brand-search" placeholder="Search in Brands">
+        </div>`;
+
+    if (brandsDiv) {
+        brandsDiv.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    const listSearchElements = document.querySelectorAll('.brand-search');
+    listSearchElements.forEach(el => {
+        el.addEventListener('change', asideBrandSearch);
+        el.addEventListener('keyup', asideBrandSearch);
+    });
+});
+
+function removeDiacritics(str) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+function asideBrandSearch(el) {
+    let input, filter, brands, div, a, i, txtValue;
+    input = el.target;
+    filter = removeDiacritics(input.value).toUpperCase();
+    brands = el.target.parentNode.parentNode.querySelector('.aside-item-list-container .brands');
+    brandChoices = brands.querySelectorAll('.brand_channel_choice');
+
+    for (const div of brandChoices) {
+        const label = div.getElementsByTagName('label')[0];
+        const txtValue = label.textContent || label.innerText;
+        div.style.display = removeDiacritics(txtValue).toUpperCase().includes(filter) ?
+            '' :
+            'none';
+    }
+}
+
 function openChannelList(input) {
     const brandContainer = input.closest('.brand-container');
 
