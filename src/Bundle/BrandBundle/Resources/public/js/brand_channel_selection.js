37a0c98e396d9863bbb361f321b222216b0f114d
Bas <bas@twindigital.nl>
2023-09-17 12:16:37 +0200

    Added a toggle to show channels from a brand to improve ux

--- src/Bundle/BrandBundle/Resources/public/js/brand_channel_selection.js
+++ src/Bundle/BrandBundle/Resources/public/js/brand_channel_selection.js
@@ -1,7 +1,35 @@
 document.querySelectorAll('.brands input.brand-choice').forEach(
     (brandCheckbox) => brandCheckbox.addEventListener('change', () => brandCheckbox
         .closest('.brand-container')
-        .querySelectorAll('.brand-channels')
-        .forEach((brandChannels) => brandChannels.style.display = brandCheckbox.checked ? 'block' : 'none')
+        .querySelectorAll('.publication-channel-toggle-button')
+        .forEach((brandChannelsToggle) => brandChannelsToggle.style.display = brandCheckbox.checked ? 'flex' : 'none')
     ) || brandCheckbox.dispatchEvent(new Event('change'))
 );
+
+function openChannelList(input) {
+    const brandContainer = input.closest('.brand-container');
+
+    brandContainer.querySelectorAll('.brand-channels')
+    .forEach((brandChannels) => {
+        brandChannels.style.display = brandChannels.style.display === 'block' ? 'none' : 'block';
+    });
+
+    brandContainer.classList.toggle('show');
+}
+
+document.querySelectorAll('.brands input.brand-choice').forEach(function (input) {
+
+    const showChannels = document.createElement('a');
+    showChannels.href = '#';
+    showChannels.title = 'Toggle Channels';
+    showChannels.innerHTML = '<i class="iconoir-nav-arrow-down"></i>';
+    showChannels.className = 'publication-channel-toggle-button';
+    showChannels.addEventListener('click', function (ev) {
+        openChannelList(input);
+        ev.preventDefault();
+    });
+    input.closest('.checkbox').insertAdjacentElement('afterend', showChannels);
+    input.showChannels = showChannels;
+
+});
+
