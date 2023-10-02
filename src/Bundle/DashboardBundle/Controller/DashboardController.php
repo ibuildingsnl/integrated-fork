f778acc277369bcc3884b3c8ff765bf87039bd33
mrgotier256 <48410942+mrgotier256@users.noreply.github.com>
2023-10-02 10:56:21 +0200

    Set channel by select element
    Add a select menu to change current channel

--- src/Bundle/DashboardBundle/Controller/DashboardController.php
+++ src/Bundle/DashboardBundle/Controller/DashboardController.php
@@ -10,22 +10,46 @@
  */
 
 namespace Integrated\Bundle\DashboardBundle\Controller;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Repository\DocumentRepository;
+use Integrated\Bundle\ContentBundle\Document\Channel\Channel;
+use Integrated\Bundle\ContentBundle\Document\Content\Article;
 use Integrated\Bundle\IntegratedBundle\Controller\AbstractController;
 use Integrated\Common\Content\Channel\ChannelContextInterface;
 use Symfony\Component\HttpFoundation\Response;
+use \Integrated\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class DashboardController extends AbstractController
 {
 
-    public function __construct(private readonly ChannelContextInterface $channelContext){}
-
-    public function index(string $searchSelection = 'all'): Response
+    public function __construct(private readonly ChannelContextInterface $channelContext, private readonly DocumentManager $manager)
     {
-            return $this->render('@IntegratedDashboard/index.html.twig',[
-            'controller_name' => 'DashboardController',
-            "channel" => $this->channelContext->getChannel()->getName()
+    }
+
+    public function index(Request $request): Response
+    {
+        // Get the list of articles
+        $mostRecentArticles = $this->manager->getRepository(Article::class)->findBy([], ['publishTime.startDate' => 'DESC'], limit: 10);
+
+        $selectChannelForm = $this->createForm(ChannelChoiceType::class, null, [
+            'multiple' => false,
+            'return_object' => true, // true = object, false = ID
         ]);
 
+        $selectedByFormChannel = $request->query->get('integrated_channel_choice');
+        $objectChannel = $this->manager->getRepository(Channel::class)->find($selectedByFormChannel);
+        $objectChannel !== null ? $this->channelContext->setChannel($objectChannel) : null;
+
+        // Render the view with the data
+        return $this->render('@IntegratedDashboard/index.html.twig', [
+            "channelName" => $this->channelContext->getChannel()->getName(),
+            "mostRecentArticles" => $mostRecentArticles,
+            "nbrOfArticles" => count($mostRecentArticles),
+            "channelForm" => $selectChannelForm->createView(),
+        ]);
     }
 }
