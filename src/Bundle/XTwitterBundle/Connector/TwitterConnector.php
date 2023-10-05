2467141776f7a1d3e3860103080ef27c35333ef6
jesse <jesse@twindigital.nl>
2023-10-05 15:43:02 +0200

    Make twitter connector work

--- src/Bundle/XTwitterBundle/Connector/TwitterConnector.php
+++ src/Bundle/XTwitterBundle/Connector/TwitterConnector.php
@@ -29,16 +29,15 @@ final class TwitterConnector implements ConnectorInterface
             throw new CouldNotPublish('An access token and secret are required to create a twitter exporter');
         }
 
-        dd($settings, $content);
         $client = $this->factory->createClient($options->get('token'), $options->get('token_secret'));
 
-        $message = $settings['foo'] ?? '';
-        if (!empty($message)) {
-            $message .= "\n\n";
-        }
-        $message .= "https://".$this->linkMaker->urlFor($content, $channel);
+        $message = [
+            $settings['title'] ?? null,
+            $settings['text'] ?? null,
+            $this->linkMaker->urlFor($content, $channel),
+        ];
 
-        $response = $client->post('tweets', ['text' => $message], true);
+        $response = $client->post('tweets', ['text' => implode("\n\n", $message)], true);
 
         if (!isset($response['data']['id'])) {
             throw new CouldNotPublish('Could not publish to twitter: ' . $this->getErrorFromResponse($response));
