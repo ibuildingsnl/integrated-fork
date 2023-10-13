fb9b56ff24b3b05faee2f1f89cee34dc645af62f
jesse <jesse@twindigital.nl>
2023-10-13 13:46:26 +0200

    Update to new(er) channel interface

--- src/Bundle/LinkedInBundle/Connector/LinkedInConnector.php
+++ src/Bundle/LinkedInBundle/Connector/LinkedInConnector.php
@@ -6,8 +6,8 @@ use Integrated\Bundle\ChannelBundle\Model\ConnectorInterface;
 use Integrated\Bundle\ChannelBundle\Model\CouldNotPublish;
 use Integrated\Bundle\ChannelBundle\Services\LinkMaker;
 use Integrated\Bundle\ContentBundle\Document\Content\Content;
-use Integrated\Common\Channel\ChannelInterface;
 use Integrated\Common\Channel\Connector\Config\OptionsInterface;
+use Integrated\Common\Content\Channel\ChannelInterface;
 
 final class LinkedInConnector implements ConnectorInterface
 {
