01081d34bfb26a9ab87ea608836bda3f70d7d0f7
jesse <jesse@twindigital.nl>
2023-10-13 13:39:28 +0200

    Properly show/hide exclusive channel aspects

--- src/Bundle/ContentBundle/Resources/public/js/channel_types.js
+++ src/Bundle/ContentBundle/Resources/public/js/channel_types.js
@@ -2,8 +2,8 @@ function showHideExclusiveElements()
 {
     const type = document.querySelector('#channel_type')?.value;
     document.querySelectorAll('[data-exclusive-to]').forEach(function (e) {
-        // alert(e.dataset.exclusiveTo + ' ?== ' + type);
         e.style.display = e.dataset.exclusiveTo === type ? 'inherit' : 'none';
     })
 }
 showHideExclusiveElements();
+document.querySelector('#channel_type')?.addEventListener('change', showHideExclusiveElements);
