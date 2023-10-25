fefcf3dbf74cb1bc98443f788b500600119f128c
Bas <bas@twindigital.nl>
2023-10-25 11:56:49 +0200

    Damn you, Jenkins.

--- src/Bundle/ContentBundle/Twig/Extension/JsonLDExtension.php
+++ src/Bundle/ContentBundle/Twig/Extension/JsonLDExtension.php
@@ -36,7 +36,7 @@ class JsonLDExtension extends AbstractExtension
     public function getFilters()
     {
         return [
-            new TwigFilter('json_ld', [$this, 'encode'], ['is_safe' => ['html']])
+            new TwigFilter('json_ld', [$this, 'encode'], ['is_safe' => ['html']]),
         ];
     }
 
