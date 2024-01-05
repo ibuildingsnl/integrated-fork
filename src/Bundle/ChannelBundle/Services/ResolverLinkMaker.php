fd9cbd3aca2c31b8c0a320959b83b650dfcfa55d
Bas <bas@twindigital.nl>
2024-01-05 20:34:31 +0100

    Fix linkmaker

--- src/Bundle/ChannelBundle/Services/ResolverLinkMaker.php
+++ src/Bundle/ChannelBundle/Services/ResolverLinkMaker.php
@@ -2,6 +2,7 @@
 
 namespace Integrated\Bundle\ChannelBundle\Services;
 
+use Integrated\Bundle\BrandBundle\Document\BrandRepository;
 use Integrated\Bundle\ContentBundle\Document\Content\Content;
 use Integrated\Bundle\PageBundle\Services\UrlResolver;
 use Integrated\Common\Content\Channel\ChannelInterface;
@@ -10,11 +11,26 @@ final class ResolverLinkMaker implements LinkMaker
 {
     public function __construct(
         private readonly UrlResolver $urlResolver,
+        private readonly BrandRepository $brands,
     ) {
     }
 
     public function urlFor(Content $content, ChannelInterface $preferredChannel): string
     {
-        return $this->urlResolver->generateUrl($content, $preferredChannel->getId());
+        foreach ($this->brands->all() as $brand) {
+            if ($brand->hasChannel($preferredChannel)) {
+                $channel = $brand->getWebsiteChannel();
+            }
+        }
+
+        if ($channel instanceof ChannelInterface) {
+            return (
+                   $content->hasChannel($channel)
+                       ? $channel->getPrimaryDomain()
+                       : $content->getPrimaryChannel()->getPrimaryDomain()
+                   ) . $this->urlResolver->generateUrl($content, $channel->getId());
+        } else {
+            return '';
+        }
     }
 }
