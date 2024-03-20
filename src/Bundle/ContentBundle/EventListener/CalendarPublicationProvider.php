7ee247d5fda87bbbb2acb43bd3db67ff03e163ad
Bas <bas@twindigital.nl>
2024-03-20 23:10:32 +0100

    Added Calendar support for social posts

---
+++ src/Bundle/ContentBundle/EventListener/CalendarPublicationProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Integrated\Bundle\ContentBundle\EventListener;
+
+use DateTimeImmutable;
+use Integrated\Bundle\AssetBundle\Manager\AssetManager;
+use Integrated\Bundle\BrandBundle\Document\BrandRepository;
+use Integrated\Bundle\ContentBundle\Document\Content\PublicationRepositoryInterface;
+use Integrated\Bundle\ContentBundle\Event\CalendarEvent;
+use Integrated\Common\Content\Channel\ChannelInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CalendarPublicationProvider implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly AssetManager $js,
+        private readonly PublicationRepositoryInterface $publicationRepository,
+        private readonly BrandRepository $brands,
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            CalendarEvent::PREPARED_WEEK_OPTIONS => 'addPublicationSchedule',
+            CalendarEvent::PREPARED_MONTH_OPTIONS => 'addPublicationSchedule',
+        ];
+    }
+
+    public function addPublicationSchedule(CalendarEvent $event): void
+    {
+        /** @var DateTimeImmutable $calendarStart */
+        $calendarStart = $event->options['start'];
+        /** @var DateTimeImmutable $calendarEnd */
+        $calendarEnd = $event->options['end'];
+
+        $publications = $this->publicationRepository->forDateRange($calendarStart, $calendarEnd);
+
+        $now = new \DateTime();
+
+        $scheduledPublications = [];
+
+        foreach ($publications as $publication) {
+            $type = $publication->getChannel()->getType();
+            $dateTime = $publication->getTime()->getStartDate();
+
+            if ($publication->getChannel() instanceof ChannelInterface) {
+                foreach ($this->brands->all() as $brand) {
+                    if ($brand->hasChannel($publication->getChannel())) {
+                        $currentBrand = $brand;
+                        $brandProfile = $brand->profile;
+                    }
+                }
+            } else {
+                continue;
+            }
+
+            $data = [
+                'id' => $publication->getContent()->getId(),
+                'title' => $publication->getContent()->getTitle(),
+                'type' => $type->getId(),
+                'name' => $type->getName(),
+                'icon' => $type->getIcon() ?: 'empty-page',
+                'published' => $now > $publication->getTime()->getStartDate() ? 'published' : 'planned',
+                'date' => $dateTime->format('Y/m/d'),
+                'time' => $dateTime->format('Hi'),
+                'display_time' => $dateTime->format('H:i'),
+                'brand_name' => $currentBrand->getName(),
+                'brand_favicon' => $brandProfile->getFavicon()?->getFile()->getPathname(),
+                'brand_color' => $brandProfile->getColor(),
+            ];
+
+            $scheduledPublications[] = $data;
+        }
+        $this->js->add('const publicationSchedule = ' . json_encode($scheduledPublications), true);
+        $this->js->add('bundles/integratedcontent/js/publication_calendar.js');
+    }
+}
