d08e338aed39583b86ab2720c88cdc38037bbcb5
Bas <bas@twindigital.nl>
2024-04-03 12:44:33 +0200

    Improved Linkmaker

--- src/Bundle/ChannelBundle/Services/ResolverLinkMaker.php
+++ src/Bundle/ChannelBundle/Services/ResolverLinkMaker.php
@@ -4,6 +4,7 @@ namespace Integrated\Bundle\ChannelBundle\Services;
 
 use Integrated\Bundle\BrandBundle\Document\BrandRepository;
 use Integrated\Bundle\ContentBundle\Document\Content\Content;
+use Integrated\Bundle\ContentBundle\Document\Content\PublicationRepositoryInterface;
 use Integrated\Bundle\PageBundle\Services\UrlResolver;
 use Integrated\Common\Content\Channel\ChannelInterface;
 
@@ -12,15 +13,37 @@ final class ResolverLinkMaker implements LinkMaker
     public function __construct(
         private readonly UrlResolver $urlResolver,
         private readonly BrandRepository $brands,
+        private readonly PublicationRepositoryInterface $publications,
     ) {
     }
 
     public function urlFor(Content $content, ?ChannelInterface $preferredChannel): string
     {
         $channel = false;
+        $websitePublicationFound = false;
+        $websitePublicationFoundForChannel = true;
 
         if ($preferredChannel === null) {
-            $channel = $content->getPrimaryChannel();
+            $preferredChannel = $content->getPrimaryChannel();
+        }
+
+        foreach ($this->publications->forContent($content) as $publication) {
+           if ($publication->getChannel()->getType() === 'Website') {
+               if ($publication->getTime()->getStartDate() < new \DateTime('now')) {
+                   $websitePublicationFound = true;
+                   if ($publication->getChannel() === $preferredChannel) {
+                       $websitePublicationFoundForChannel = true;
+                   }
+               }
+           }
+        }
+
+        if (!$websitePublicationFound) {
+            return '';
+        }
+
+        if (!$websitePublicationFoundForChannel) {
+            $preferredChannel = $content->getPrimaryChannel();
         }
 
         foreach ($this->brands->all() as $brand) {
