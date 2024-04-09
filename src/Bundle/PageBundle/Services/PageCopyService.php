45de363d0cf5474f9c36085cce8b7d8e0eae3065
Bas <bas@twindigital.nl>
2024-04-09 22:53:49 +0200

    Jenkins

--- src/Bundle/PageBundle/Services/PageCopyService.php
+++ src/Bundle/PageBundle/Services/PageCopyService.php
@@ -59,7 +59,7 @@ class PageCopyService
 
         /** @var Page $page */
         foreach ($result as $page) {
-            if (isset($data['pages']['page' . $page->getId()]['selected']) && $data['pages']['page' . $page->getId()]['selected'] === true) {
+            if (isset($data['pages']['page'.$page->getId()]['selected']) && $data['pages']['page'.$page->getId()]['selected'] === true) {
                 $existingPage = $this->documentManager->getRepository(Page::class)->findOneBy(
                     [
                         'channel.$id' => $targetChannel->getId(),
@@ -79,7 +79,7 @@ class PageCopyService
                 $copiedPage->setChannel($targetChannel);
 
                 foreach ($copiedPage->getGrids() as $key => $grid) {
-                    $this->copyGridBlocks($grid, $data['pages']['page' . $page->getId()]['blocks'], $copiedPage);
+                    $this->copyGridBlocks($grid, $data['pages']['page'.$page->getId()]['blocks'], $copiedPage);
                 }
 
                 $this->documentManager->persist($copiedPage);
@@ -105,8 +105,8 @@ class PageCopyService
 
             if ($block instanceof Block) {
                 // copy block
-                if (isset($data['block_' . $block->getId()]['operation']) && $data['block_' . $block->getId(
-                    )]['operation'] == 'clone') {
+                if (isset($data['block_'.$block->getId()]['operation']) && $data['block_'.$block->getId(
+                )]['operation'] == 'clone') {
                     $classMetadata = $this->documentManager->getClassMetadata(\get_class($block));
 
                     $className = $classMetadata->getName();
@@ -122,7 +122,6 @@ class PageCopyService
                     $getters = [];
                     $setters = [];
 
-
                     foreach ($reflector->getMethods() as $method) {
                         $methodName = $method->getName();
                         if (strpos($methodName, 'get') === 0 && $method->getNumberOfParameters() === 0) {
@@ -134,7 +133,7 @@ class PageCopyService
                     }
 
                     foreach ($getters as $getter) {
-                        $setter = 'set' . substr($getter, 3);
+                        $setter = 'set'.substr($getter, 3);
 
                         if (\in_array($setter, $setters)) {
                             $value = $block->$getter();
@@ -142,7 +141,7 @@ class PageCopyService
                         }
                     }
 
-                    $copiedBlock->setId($data['block_' . $block->getId()]['newBlockId']);
+                    $copiedBlock->setId($data['block_'.$block->getId()]['newBlockId']);
                     $copiedBlock->setCreatedAt(new \DateTime());
 
                     $this->documentManager->persist($copiedBlock);
