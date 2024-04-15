a1eeab67d9d15db1175490b59796fc6fb0b23b61
Bas <bas@twindigital.nl>
2024-04-15 11:39:52 +0200

    When there is no existing publication, images would not be saved

--- src/Bundle/ContentBundle/EventListener/ContentPublicationIntegrationListener.php
+++ src/Bundle/ContentBundle/EventListener/ContentPublicationIntegrationListener.php
@@ -81,11 +81,6 @@ class ContentPublicationIntegrationListener implements EventSubscriberInterface
 
                 $existingChannelPublications = $this->publications->forContentOnChannel($content, $channel);
 
-                if (!$existingChannelPublications) {
-                    $this->publications->add(new Publication($content, $channel, $time, \is_array($data) ? $data : []));
-                    continue;
-                }
-
                 $imagesProcessed = [];
                 if (isset($data['images']) && \is_array($data['images'])) {
                     foreach ($data['images'] as $key => $image) {
@@ -96,6 +91,11 @@ class ContentPublicationIntegrationListener implements EventSubscriberInterface
                         ];
                     }
                     $data['images'] = $imagesProcessed;
+                }
+
+                if (!$existingChannelPublications) {
+                    $this->publications->add(new Publication($content, $channel, $time, \is_array($data) ? $data : []));
+                    continue;
                 }
 
                 foreach ($existingPublications as $key => $existingPublication) {
