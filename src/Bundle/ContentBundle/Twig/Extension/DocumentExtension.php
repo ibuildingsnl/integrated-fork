e6a646162e26e1c0f3b33cf10ae9620facb19cb9
Bas <bas@twindigital.nl>
2024-04-22 13:17:26 +0200

    used u.truncate

--- src/Bundle/ContentBundle/Twig/Extension/DocumentExtension.php
+++ src/Bundle/ContentBundle/Twig/Extension/DocumentExtension.php
@@ -7,7 +7,6 @@ use Integrated\Bundle\ContentBundle\Document\Content\Content;
 use Integrated\Common\Content\ContentInterface;
 use Solarium\QueryType\Select\Result\Document;
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class DocumentExtension extends AbstractExtension
@@ -29,7 +28,6 @@ class DocumentExtension extends AbstractExtension
     {
         return [
             new TwigFunction('integrated_document', [$this, 'getDocument']),
-            new TwigFilter('safeTruncate', [$this, 'safeTruncate']),
         ];
     }
 
@@ -56,22 +54,5 @@ class DocumentExtension extends AbstractExtension
         }
 
         return null;
-    }
-
-    public function safeTruncate($string, $length)
-    {
-        if (mb_strlen($string) <= $length) {
-            return $string;
-        }
-
-        $truncated = mb_substr($string, 0, $length);
-
-        if (preg_match('/&[^;]{0,4}$/', $truncated)) {
-            $truncated = preg_replace('/&[^;]{0,4}$/', '', $truncated);
-        }
-
-        $truncated = html_entity_decode($truncated, ENT_QUOTES, 'UTF-8');
-
-        return $truncated . '...';
     }
 }
