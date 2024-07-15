fb432385c912dc59562692cf605026756ffb7cb4
Bas <bas@twindigital.nl>
2024-07-15 09:40:41 +0200

    $item can be a File instead of an array when there is a single result.

--- src/Bundle/ContentBundle/Form/DataTransformer/MultipleFileTransformer.php
+++ src/Bundle/ContentBundle/Form/DataTransformer/MultipleFileTransformer.php
@@ -12,6 +12,7 @@
 namespace Integrated\Bundle\ContentBundle\Form\DataTransformer;
 
 use Doctrine\ODM\MongoDB\Repository\DocumentRepository;
+use Integrated\Bundle\ContentBundle\Document\Content\File;
 use Symfony\Component\Form\DataTransformerInterface;
 
 /**
@@ -37,7 +38,7 @@ class MultipleFileTransformer implements DataTransformerInterface
         if (\is_array($list)) {
             $data = [];
             foreach ($list as $item) {
-                if (\count($item) > 0) {
+                if ($item instanceof File || \count($item) > 0) {
                     if (\is_array($item)) {
                         $data[] = $item['$id'];
                     } else {
