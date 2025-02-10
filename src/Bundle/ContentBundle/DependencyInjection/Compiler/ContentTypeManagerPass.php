e7266b6a50fec45fa147ea94f55bfaec7448837d
Marijn Otte <marijn@e-active.nl>
2025-02-10 10:37:38 +0100

    Allow content_types.xml in project config directory

--- src/Bundle/ContentBundle/DependencyInjection/Compiler/ContentTypeManagerPass.php
+++ src/Bundle/ContentBundle/DependencyInjection/Compiler/ContentTypeManagerPass.php
@@ -31,5 +31,9 @@ class ContentTypeManagerPass implements CompilerPassInterface
                 $container->getDefinition(self::SERVICE_ID)->addMethodCall('registerFile', [$file]);
             }
         }
+
+        if (is_file($file = $container->getParameter('kernel.project_dir') . '/config/integrated/content_types.xml')) {
+            $container->getDefinition(self::SERVICE_ID)->addMethodCall('registerFile', [$file]);
+        }
     }
 }
