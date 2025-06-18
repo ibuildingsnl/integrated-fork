412fbbcfc2eaa1a4e3f62285456b888d8567bac9
Bas <bas@twindigital.nl>
2025-06-18 16:39:22 +0200

    only filter on channel if it's not an image or a file

--- src/Bundle/ContentBundle/Controller/ArticleSearchController.php
+++ src/Bundle/ContentBundle/Controller/ArticleSearchController.php
@@ -99,13 +99,20 @@ class ArticleSearchController extends AbstractController
         /** @var Channel $channel */
         $channel = $this->documentManager->getRepository(Channel::class)->find($channelId);
 
+        $criteria = [
+            'contenttypes' => explode(',', $contentTypeIds),
+            'sort' => 'rel',
+            'q' => $q,
+        ];
+
+        $contentTypes = explode(',', $contentTypeIds);
+
+        if (!in_array('image', $contentTypes) || !in_array('file', $contentTypes)) {
+            $criteria['channels'] = explode(',', $channelId);
+        }
+
         $query = $this->queryFactory
-            ->createQuery(IntegratedContent::class, [
-                'contenttypes' => explode(',', $contentTypeIds),
-                'channels' => explode(',', $channelId),
-                'sort' => 'rel',
-                'q' => $q,
-            ])
+            ->createQuery(IntegratedContent::class, $criteria)
             ->getQuery();
 
         /** @var Document[] $items */
