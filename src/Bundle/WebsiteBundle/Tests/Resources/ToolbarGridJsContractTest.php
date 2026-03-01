56ee5428674355e21f3812a1792ecdccdd2acdfb
Bas <bas@twindigital.nl>
2026-03-01 11:15:01 +0100

    perf(editor): reduce drag-drop jank in grid builder

---
+++ src/Bundle/WebsiteBundle/Tests/Resources/ToolbarGridJsContractTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integrated\Bundle\WebsiteBundle\Tests\Resources;
+
+use PHPUnit\Framework\TestCase;
+
+final class ToolbarGridJsContractTest extends TestCase
+{
+    public function testGridEditorScriptUsesSortableGuardAndThrottledHistorySnapshots(): void
+    {
+        $path = __DIR__.'/../../Resources/views/themes/default/objects/toolbar-grid-js.html.twig';
+        $content = (string) file_get_contents($path);
+
+        self::assertStringContainsString('__integratedSortableInstance', $content);
+        self::assertStringContainsString('animation: 120', $content);
+        self::assertStringContainsString('delayOnTouchOnly: true', $content);
+        self::assertStringContainsString('rememberStateNow', $content);
+        self::assertStringContainsString('flushRememberState', $content);
+    }
+}
+
