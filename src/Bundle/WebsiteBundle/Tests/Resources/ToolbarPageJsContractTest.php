2f87ac348402062dc4a7d8ba4952ec1f0f875be0
Bas <bas@twindigital.nl>
2026-03-01 10:30:32 +0100

    feat(pagebuilder-v2): switch website editor save flow to v2 endpoint

---
+++ src/Bundle/WebsiteBundle/Tests/Resources/ToolbarPageJsContractTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integrated\Bundle\WebsiteBundle\Tests\Resources;
+
+use PHPUnit\Framework\TestCase;
+
+final class ToolbarPageJsContractTest extends TestCase
+{
+    public function testUsesPageBuilderSaveRouteAndPayloadKeys(): void
+    {
+        $path = __DIR__.'/../../Resources/views/themes/default/objects/toolbar-page-js.html.twig';
+        $content = (string) file_get_contents($path);
+
+        self::assertStringContainsString("integrated_website_pagebuilder_save", $content);
+        self::assertStringContainsString("'layoutVersion': 2", $content);
+        self::assertStringContainsString("'payload':", $content);
+    }
+}
+
