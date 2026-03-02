fb2051361a1d7b9ed7e176dc72d5cf80d54dc73a
Bas <bas@twindigital.nl>
2026-03-02 12:35:08 +0100

    Show flash notification after media sidebar save

---
+++ src/Bundle/ContentBundle/Tests/Resources/ContentEditIframeTurboStreamFlowTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integrated\Bundle\ContentBundle\Tests\Resources;
+
+use PHPUnit\Framework\TestCase;
+
+class ContentEditIframeTurboStreamFlowTest extends TestCase
+{
+    public function testIframeTurboStreamTemplateIncludesFlashMessages(): void
+    {
+        $template = file_get_contents(__DIR__.'/../../Resources/views/content/edit.iframe.turbo_stream.html.twig');
+
+        $this->assertIsString($template);
+        $this->assertStringContainsString("@IntegratedContent/content/flash.turbo_stream.html.twig", $template);
+        $this->assertStringContainsString('target="media-edit-panel"', $template);
+    }
+}
+
