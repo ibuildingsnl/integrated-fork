90576a7ad7d42755004e3fa6c6bddc603050eece
Bas <bas@twindigital.nl>
2026-03-10 18:02:51 +0100

    refactor(content): add facet filter icons to aside panels

--- src/Bundle/ContentBundle/Tests/Resources/ContentBaseTemplateComponentsTest.php
+++ src/Bundle/ContentBundle/Tests/Resources/ContentBaseTemplateComponentsTest.php
@@ -17,4 +17,19 @@ final class ContentBaseTemplateComponentsTest extends TestCase
         self::assertStringContainsString("component('integrated_admin:options_toolbar'", $template);
         self::assertStringContainsString("component('integrated_admin:aside_panel'", $template);
     }
+
+    public function testContentBaseProvidesIconsForFacetPanels(): void
+    {
+        $template = file_get_contents(__DIR__.'/../../Resources/views/content/base.html.twig');
+
+        self::assertIsString($template);
+        self::assertStringContainsString("{% set facetIcon = 'folder' %}", $template);
+        self::assertStringContainsString("{% set facetIcon = 'network-reverse' %}", $template);
+        self::assertStringContainsString("{% set facetIcon = 'empty-page' %}", $template);
+        self::assertStringContainsString("{% set facetIcon = 'info-circle' %}", $template);
+        self::assertStringContainsString("{% set facetIcon = 'user' %}", $template);
+        self::assertStringContainsString("{% set facetIcon = 'edit-pencil' %}", $template);
+        self::assertStringContainsString("icon: facetIcon", $template);
+        self::assertStringContainsString("icon: 'link'", $template);
+    }
 }
