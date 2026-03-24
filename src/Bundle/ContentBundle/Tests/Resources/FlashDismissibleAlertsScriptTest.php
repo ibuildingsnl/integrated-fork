fce81b534e2918cfc4ef5bb47fd4b7b84cea70b8
Bas <bas@twindigital.nl>
2026-03-24 13:48:31 +0100

    bugfixes and public files

---
+++ src/Bundle/ContentBundle/Tests/Resources/FlashDismissibleAlertsScriptTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integrated\Bundle\ContentBundle\Tests\Resources;
+
+use PHPUnit\Framework\TestCase;
+
+final class FlashDismissibleAlertsScriptTest extends TestCase
+{
+    public function testGlobalScriptObservesDynamicFlashMessagesForAutoDismiss(): void
+    {
+        $script = file_get_contents(__DIR__.'/../../Resources/assets/js/global.js');
+
+        $this->assertIsString($script);
+        $this->assertStringContainsString('new MutationObserver', $script);
+        $this->assertStringContainsString("document.getElementById('flash-messages')", $script);
+        $this->assertStringContainsString('dismissibleAlertBound', $script);
+        $this->assertStringContainsString('DISMISSIBLE_ALERT_TIMEOUT_MS', $script);
+    }
+}
