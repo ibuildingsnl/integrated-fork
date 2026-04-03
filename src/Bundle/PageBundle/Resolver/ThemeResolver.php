1f9b00ce1013f48faa6bf8a3ec72abfca2c5cf28
Bas <bas@twindigital.nl>
2026-04-03 22:12:56 +0200

    Cache theme resolution per channel

--- src/Bundle/PageBundle/Resolver/ThemeResolver.php
+++ src/Bundle/PageBundle/Resolver/ThemeResolver.php
@@ -14,12 +14,16 @@ namespace Integrated\Bundle\PageBundle\Resolver;
 use Integrated\Bundle\ThemeBundle\Templating\ThemeManager;
 use Integrated\Common\Channel\Connector\Config\ResolverInterface;
 use Integrated\Common\Content\Channel\ChannelInterface;
+use Psr\Cache\CacheItemPoolInterface;
 
 /**
  * @author Ger Jan van den Bosch <gerjan@e-active.nl>
  */
 class ThemeResolver
 {
+    private const THEME_CACHE_KEY_PREFIX = 'integrated_page_theme_';
+    private const THEME_CACHE_TTL_SECONDS = 300;
+
     /**
      * @var ResolverInterface
      */
@@ -30,10 +34,16 @@ class ThemeResolver
      */
     private $themeManager;
 
-    public function __construct(ResolverInterface $resolver, ThemeManager $themeManager)
+    /**
+     * @var CacheItemPoolInterface|null
+     */
+    private $cache;
+
+    public function __construct(ResolverInterface $resolver, ThemeManager $themeManager, ?CacheItemPoolInterface $cache = null)
     {
         $this->resolver = $resolver;
         $this->themeManager = $themeManager;
+        $this->cache = $cache;
     }
 
     /**
@@ -41,18 +51,79 @@ class ThemeResolver
      */
     public function getTheme(ChannelInterface $channel)
     {
+        $channelId = trim((string) $channel->getId());
+
+        if ('' !== $channelId) {
+            $cachedTheme = $this->findThemeFromCache($channelId);
+            if (null !== $cachedTheme) {
+                return $cachedTheme;
+            }
+        }
+
         if ($configs = $this->resolver->getConfigs($channel)) {
             foreach ($configs as $config) {
                 if ($config->getAdapter() === 'website') {
                     $theme = $config->getOptions()->get('theme');
 
                     if ($this->themeManager->hasTheme($theme)) {
+                        if ('' !== $channelId) {
+                            $this->saveThemeToCache($channelId, $theme);
+                        }
+
                         return $theme;
                     }
                 }
             }
         }
 
+        if ('' !== $channelId) {
+            $this->saveThemeToCache($channelId, 'default');
+        }
+
         return 'default';
+    }
+
+    private function findThemeFromCache(string $channelId): ?string
+    {
+        if (null === $this->cache) {
+            return null;
+        }
+
+        $cacheItem = $this->cache->getItem($this->getThemeCacheKey($channelId));
+        if (!$cacheItem->isHit()) {
+            return null;
+        }
+
+        $theme = trim((string) $cacheItem->get());
+        if ('' === $theme) {
+            $this->cache->deleteItem($cacheItem->getKey());
+
+            return null;
+        }
+
+        if (!$this->themeManager->hasTheme($theme) && $theme !== 'default') {
+            $this->cache->deleteItem($cacheItem->getKey());
+
+            return null;
+        }
+
+        return $theme;
+    }
+
+    private function saveThemeToCache(string $channelId, string $theme): void
+    {
+        if (null === $this->cache) {
+            return;
+        }
+
+        $cacheItem = $this->cache->getItem($this->getThemeCacheKey($channelId));
+        $cacheItem->set($theme);
+        $cacheItem->expiresAfter(self::THEME_CACHE_TTL_SECONDS);
+        $this->cache->save($cacheItem);
+    }
+
+    private function getThemeCacheKey(string $channelId): string
+    {
+        return self::THEME_CACHE_KEY_PREFIX.md5($channelId);
     }
 }
---
+++ src/Bundle/PageBundle/Tests/Resolver/ThemeResolverTest.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integrated\Bundle\PageBundle\Tests\Resolver;
+
+use Integrated\Bundle\PageBundle\Resolver\ThemeResolver;
+use Integrated\Bundle\ThemeBundle\Templating\ThemeManager;
+use Integrated\Common\Channel\Connector\Config\ResolverInterface;
+use Integrated\Common\Content\Channel\ChannelInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+
+class ThemeResolverTest extends TestCase
+{
+    public function testGetThemeCachesResolvedThemeForChannel(): void
+    {
+        $cache = new ArrayAdapter();
+        $channel = $this->createChannel('bakkersinbedrijf');
+        $themeManager = $this->createThemeManager(['twindigital']);
+
+        $resolver = $this->createMock(ResolverInterface::class);
+        $resolver
+            ->expects($this->once())
+            ->method('getConfigs')
+            ->with($channel)
+            ->willReturn([
+                $this->createConfig('website', 'twindigital'),
+            ]);
+
+        $themeResolver = new ThemeResolver($resolver, $themeManager, $cache);
+
+        self::assertSame('twindigital', $themeResolver->getTheme($channel));
+        self::assertSame('twindigital', $themeResolver->getTheme($channel));
+    }
+
+    public function testGetThemeCachesDefaultWhenNoValidWebsiteThemeExists(): void
+    {
+        $cache = new ArrayAdapter();
+        $channel = $this->createChannel('bakkersinbedrijf');
+        $themeManager = $this->createThemeManager(['twindigital']);
+
+        $resolver = $this->createMock(ResolverInterface::class);
+        $resolver
+            ->expects($this->once())
+            ->method('getConfigs')
+            ->with($channel)
+            ->willReturn([
+                $this->createConfig('website', 'unknown-theme'),
+            ]);
+
+        $themeResolver = new ThemeResolver($resolver, $themeManager, $cache);
+
+        self::assertSame('default', $themeResolver->getTheme($channel));
+        self::assertSame('default', $themeResolver->getTheme($channel));
+    }
+
+    public function testGetThemeUsesPersistentCacheAcrossResolverInstances(): void
+    {
+        $cache = new ArrayAdapter();
+        $channel = $this->createChannel('bakkersinbedrijf');
+        $themeManager = $this->createThemeManager(['twindigital']);
+
+        $warmupResolver = $this->createMock(ResolverInterface::class);
+        $warmupResolver
+            ->expects($this->once())
+            ->method('getConfigs')
+            ->with($channel)
+            ->willReturn([
+                $this->createConfig('website', 'twindigital'),
+            ]);
+
+        $warmupThemeResolver = new ThemeResolver($warmupResolver, $themeManager, $cache);
+        self::assertSame('twindigital', $warmupThemeResolver->getTheme($channel));
+
+        $cachedResolver = $this->createMock(ResolverInterface::class);
+        $cachedResolver
+            ->expects($this->never())
+            ->method('getConfigs');
+
+        $cachedThemeResolver = new ThemeResolver($cachedResolver, $themeManager, $cache);
+        self::assertSame('twindigital', $cachedThemeResolver->getTheme($channel));
+    }
+
+    private function createChannel(string $id): ChannelInterface
+    {
+        $channel = $this->createMock(ChannelInterface::class);
+        $channel->method('getId')->willReturn($id);
+
+        return $channel;
+    }
+
+    /**
+     * @param array<int, string> $themes
+     */
+    private function createThemeManager(array $themes): ThemeManager
+    {
+        $manager = $this->createMock(ThemeManager::class);
+        $manager
+            ->method('hasTheme')
+            ->willReturnCallback(static fn (?string $theme): bool => \in_array((string) $theme, $themes, true));
+
+        return $manager;
+    }
+
+    private function createConfig(string $adapter, string $theme): object
+    {
+        return new class($adapter, $theme) {
+            private string $adapter;
+            private object $options;
+
+            public function __construct(string $adapter, string $theme)
+            {
+                $this->adapter = $adapter;
+                $this->options = new class($theme) {
+                    private string $theme;
+
+                    public function __construct(string $theme)
+                    {
+                        $this->theme = $theme;
+                    }
+
+                    public function get(string $key): ?string
+                    {
+                        if ($key !== 'theme') {
+                            return null;
+                        }
+
+                        return $this->theme;
+                    }
+                };
+            }
+
+            public function getAdapter(): string
+            {
+                return $this->adapter;
+            }
+
+            public function getOptions(): object
+            {
+                return $this->options;
+            }
+        };
+    }
+}
