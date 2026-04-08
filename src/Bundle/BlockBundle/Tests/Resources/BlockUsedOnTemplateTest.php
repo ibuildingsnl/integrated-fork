8e080612df8d4a6bf6a3125623fac0d26df1f7a9
Bas <bas@twindigital.nl>
2026-04-08 22:51:49 +0200

    feat(block): add duplicate flow with channel-aware id suggestion

--- src/Bundle/BlockBundle/Tests/Resources/BlockUsedOnTemplateTest.php
+++ src/Bundle/BlockBundle/Tests/Resources/BlockUsedOnTemplateTest.php
@@ -20,6 +20,9 @@ final class BlockUsedOnTemplateTest extends TestCase
         self::assertStringContainsString('{% trans %}Container blocks{% endtrans %}', $template);
         self::assertStringContainsString("path('integrated_page_page_edit', {'id': page['_id']})", $template);
         self::assertStringContainsString("path('integrated_block_block_edit', {'id': containerBlock['_id']})", $template);
+        self::assertStringContainsString("path('integrated_block_block_duplicate', duplicateRouteParams)", $template);
+        self::assertStringContainsString("duplicateRouteParams = duplicateRouteParams|merge({'target_channel': duplicateTargetChannel})", $template);
+        self::assertStringContainsString('{% trans %}Duplicate{% endtrans %}', $template);
         self::assertStringContainsString("{{ templateUsage.template|default('unknown template') }}", $template);
         self::assertStringContainsString('page.channel[\'$id\']|default(null)', $template);
         self::assertStringContainsString('page._used_via_container_title is defined', $template);
@@ -42,5 +45,25 @@ final class BlockUsedOnTemplateTest extends TestCase
         self::assertStringContainsString("{{ templateUsage.template|default('unknown template') }}", $template);
         self::assertStringContainsString('page.channel[\'$id\']|default(null)', $template);
         self::assertStringContainsString('page._used_via_container_title is defined', $template);
+    }
+
+    public function testDuplicateTemplateExtendsNewFormAndShowsSourceBlockInfo(): void
+    {
+        $template = file_get_contents(__DIR__.'/../../Resources/views/block/duplicate.html.twig');
+        $newTemplate = file_get_contents(__DIR__.'/../../Resources/views/block/new.html.twig');
+
+        self::assertIsString($template);
+        self::assertIsString($newTemplate);
+        self::assertStringContainsString("{% extends '@IntegratedBlock/block/new.html.twig' %}", $template);
+        self::assertStringContainsString('{% trans %}Duplicate block{% endtrans %}', $template);
+        self::assertStringContainsString('{{ sourceBlock.id }}', $template);
+        self::assertStringContainsString('data-duplicate-target-channel', $template);
+        self::assertStringContainsString('data-id-availability-url', $template);
+        self::assertStringContainsString('data-duplicate-id-status', $template);
+        self::assertStringContainsString('scheduleAvailabilityCheck', $template);
+        self::assertStringContainsString('checkIdAvailability', $template);
+        self::assertStringContainsString('swapChannelInId', $template);
+        self::assertStringContainsString('{% block editor_heading %}', $newTemplate);
+        self::assertStringContainsString('{% block editor_intro %}', $newTemplate);
     }
 }
