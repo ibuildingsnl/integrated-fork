dc933c9b73bdf25ef9163d204d207c3353268bf1
Bas <bas@twindigital.nl>
2026-05-11 17:45:17 +0200

    Improved menu handling.

--- src/Bundle/WebsiteBundle/Tests/Resources/ToolbarMenuDirtyTrackingContractTest.php
+++ src/Bundle/WebsiteBundle/Tests/Resources/ToolbarMenuDirtyTrackingContractTest.php
@@ -19,4 +19,24 @@ final class ToolbarMenuDirtyTrackingContractTest extends TestCase
         $this->assertStringContainsString('isDirty: function(element) {', $script);
         $this->assertStringContainsString('setMenuDirty(item, false);', $script);
     }
+
+    public function testToolbarMenuScriptExposesMenuPositionControls(): void
+    {
+        $script = file_get_contents(__DIR__.'/../../Resources/views/themes/default/objects/toolbar-menu-js.html.twig');
+        $styles = file_get_contents(__DIR__.'/../../Resources/views/themes/default/objects/toolbar-css.html.twig');
+
+        $this->assertIsString($script);
+        $this->assertIsString($styles);
+        $this->assertStringContainsString('integrated-menu-setting-parent', $script);
+        $this->assertStringContainsString('integrated-menu-setting-order', $script);
+        $this->assertStringContainsString('integrated-website-menu-item-move-up', $script);
+        $this->assertStringContainsString('integrated-website-menu-item-move-down', $script);
+        $this->assertStringContainsString('integrated-website-menu-item-move-under-previous', $script);
+        $this->assertStringContainsString('integrated-website-menu-item-move-top', $script);
+        $this->assertStringContainsString('moveNodeToParent(parentNode, parentChange.value || \'\');', $script);
+        $this->assertStringContainsString('moveNodeToOrder(orderNode, orderChange.value);', $script);
+        $this->assertStringContainsString('workspaceDirty = true;', $script);
+        $this->assertStringContainsString('.integrated-menu-node-move-row', $styles);
+        $this->assertStringContainsString('.integrated-menu-node-link-action', $styles);
+    }
 }
